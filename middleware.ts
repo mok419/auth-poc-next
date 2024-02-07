d8aafdc3a51bb51844fb8dbb09ba54707699481a
Mo Kamali <mokam1997@gmail.com>
2024-02-07 13:23:28 +0000

    fixed

--- middleware.ts
+++ middleware.ts
@@ -3,11 +3,13 @@ import { updateSession } from "./lib";
 
 async function checkOtherSiteCookie(request: NextRequest) {
   const theme = request.cookies.get("theme")?.value;
+  console.log(theme)
   if (!theme) return NextResponse.json('<h2> A Theme cookie is required to access this page</h2>');
 }
 
 
 export async function middleware(request: NextRequest) {
-  await checkOtherSiteCookie(request)
-  return await updateSession(request);
+  console.log('DO WE GO HERE?')
+  await updateSession(request);
+  return await checkOtherSiteCookie(request);
 }
