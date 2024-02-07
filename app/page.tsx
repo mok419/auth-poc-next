de39651a932f423f7c31f3440ccb2094e321dfb4
Mo Kamali <mokam1997@gmail.com>
2024-02-07 13:04:00 +0000

    restrictive middleware

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,6 @@
 import { redirect } from "next/navigation";
 import { getSession, login, logout } from "@/lib";
+import Image from "next/image";
 
 export default async function Page() {
   const session = await getSession();
@@ -8,11 +9,12 @@ export default async function Page() {
       <form
         action={async (formData) => {
           "use server";
+          console.log(formData.has('email'))
           await login(formData);
           redirect("/");
         }}
       >
-        <input type="email" placeholder="Email" />
+        <input name='email' type="email" placeholder="Email" />
         <br />
         <button type="submit">Login</button>
       </form>
@@ -26,6 +28,14 @@ export default async function Page() {
         <button type="submit">Logout</button>
       </form>
       <pre>{JSON.stringify(session, null, 2)}</pre>
+      <Image
+              src="/test-image.png"
+              alt="Cookie explanation from online"
+              className="dark:invert"
+              width={600}
+              height={400}
+              priority
+            />
     </section>
   );
 }
--- middleware.ts
+++ middleware.ts
@@ -1,6 +1,13 @@
-import { NextRequest } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 import { updateSession } from "./lib";
 
+async function checkOtherSiteCookie(request: NextRequest) {
+  const theme = request.cookies.get("theme")?.value;
+  if (!theme) return NextResponse.json('<h2> A Theme cookie is required to access this page</h2>');
+}
+
+
 export async function middleware(request: NextRequest) {
+  await checkOtherSiteCookie(request)
   return await updateSession(request);
 }
