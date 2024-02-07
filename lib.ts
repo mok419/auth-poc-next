d2752687948eac80a41f3d3f19864600cb5ddf29
Mo Kamali <mokam1997@gmail.com>
2024-02-07 16:37:47 +0000

    This is site A

--- lib.ts
+++ lib.ts
@@ -26,11 +26,11 @@ export async function login(formData: FormData) {
   const user = { email: formData.get("email"), name: "John" };
 
   // Create the session
-  const expires = new Date(Date.now() + 600 * 1000);
+  const expires = new Date(Date.now() + 10 * 1000);
   const session = await encrypt({ user, expires });
 
   // Save the session in a cookie
-  cookies().set("session", session, { expires, httpOnly: true, secure: true, sameSite: "none" });
+  cookies().set("session", session, { expires, httpOnly: true });
 }
 
 export async function logout() {
@@ -57,8 +57,6 @@ export async function updateSession(request: NextRequest) {
     value: await encrypt(parsed),
     httpOnly: true,
     expires: parsed.expires,
-    sameSite: "none",
-    secure: true
   });
   return res;
 }
